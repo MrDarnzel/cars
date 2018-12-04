d1e0fb1e3725264a6bf94975fc11e40166aeb1d1
MrDarnzel <42988374+MrDarnzel@users.noreply.github.com>
2018-12-03 23:07:21 -0800

    Create cars.sh

---
+++ cars.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+#cars.sh
+#DJ Levy
+
+stop="no"
+
+while [ continue="no" ]
+do
+echo "1. Enter your  car"
+echo "2. list of cars"
+echo "Q. Quit"
+
+
+read INPUT
+
+case $INPUT in
+        1) echo "Enter in car year: "
+           read YEAR
+           echo "Enter in the make of the car: "
+           read MAKE
+           echo "Enter the model of the car: "
+           read MODEL
+           echo "$YEAR $MAKE $MODEL">>My_old_cars
+           ;;
+
+        2) sort My_old_cars
+        ;;
+
+        Q) echo "Goodbye"
+           stop="yes"
+           break
+           ;;
+esac
+
+done
